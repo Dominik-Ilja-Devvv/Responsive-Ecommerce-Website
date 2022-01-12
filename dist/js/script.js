a6f7c905140de5741032306bbcefaa1c69dcc98a
Brendon Martin <brendonmartin657@gmail.com>
2022-01-11 20:53:37 -0500

    Mobile menu can open & close. Clicked link updates the active-link class and closes menu.

--- dist/js/script.js
+++ dist/js/script.js
@@ -15,3 +15,19 @@ const showMenu = (toggleID, navID) => {
 showMenu("nav-toggle", "nav__menu");
 
 
+// ==================================================================================================== //
+// UPDATE ACTIVE LINK
+// ==================================================================================================== //
+const navLinks = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  // Add "active-link" class to clicked link
+  navLinks.forEach(link => link.classList.remove("active-link"));
+  this.classList.add("active-link");
+
+  // Close Mobile Menu
+  const navMenu = document.getElementById("nav__menu");
+  navMenu.classList.remove("show-menu");
+}
+
+navLinks.forEach(link => link.addEventListener("click", linkAction));
